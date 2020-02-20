bac5111e9f0455b49504545e67cfaeb30dd7a1a8
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-20 11:28:26 -0300

    Mudança na lógica de abrir e fechar guarda-sol

--- 1.Classes, Objetos, Abstração/P01_Projeto_Praia/src/br/com/diogo/GuardaSol.java
+++ 1.Classes, Objetos, Abstração/P01_Projeto_Praia/src/br/com/diogo/GuardaSol.java
@@ -4,12 +4,17 @@ public class GuardaSol {
 	String cor;
 	double altura;
 	double raio;
+	boolean fechado = true;
 	
 	void abrir() {
-		System.out.println("Abrindo guarda-sol");
+		if(fechado) {
+			System.out.println("Abrindo guarda-sol");
+		} 		
 	}
 	
 	void fechar() {
-		System.out.println("Fechando guarda-sol");
+		if(!fechado) {
+			System.out.println("Fechando guarda-sol");
+		}
 	}
 }
