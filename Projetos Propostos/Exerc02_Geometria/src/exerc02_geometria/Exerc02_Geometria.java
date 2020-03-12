0a7e9797d94c32233dd008f7b55e1388a7cc76be
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-12 10:42:05 -0300

    TODO 10, 11, 12

--- Projetos Propostos/Exerc02_Geometria/src/exerc02_geometria/Exerc02_Geometria.java
+++ Projetos Propostos/Exerc02_Geometria/src/exerc02_geometria/Exerc02_Geometria.java
@@ -24,10 +24,13 @@ public class Exerc02_Geometria {
         // DONE 9 comente: as atribuiÃ§Ãµes deram certo? Por quÃª?
         // Deram, já que estavam publicos
         
-        // TODO 10 crie aqui uma variÃ¡vel double de nome areaTr1
+        // TODO 10 crie aqui uma variÃ¡vel double de nome areaTr1        
         // TODO 11 atravÃ©s do objeto tr1, acesse o mÃ©todo calcArea() da classe TrianguloRetangulo 
         ////////// e armazene o valor de retorno do mÃ©todo na variÃ¡vel areaTr1
         // TODO 12 imprima o valor da Ã¡rea da seguinte forma: "Ã�rea do triÃ¢ngulo tr1: <VALOR>"
+        double areaTr1 = tr1.calcArea();
+        System.out.println("Área do Triângulo: " + areaTr1);
+        
         // TODO 13 comente: por que estÃ¡ dando erro?
         // TODO 14 comente: o que pode ser feito para resolver este erro?
         
