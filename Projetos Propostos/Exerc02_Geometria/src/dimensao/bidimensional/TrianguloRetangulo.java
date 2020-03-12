35e6cd9d973885ea2c2b0f19be9a4d69d5f8a39b
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-12 10:36:17 -0300

    TODO 2 e 3

--- Projetos Propostos/Exerc02_Geometria/src/dimensao/bidimensional/TrianguloRetangulo.java
+++ Projetos Propostos/Exerc02_Geometria/src/dimensao/bidimensional/TrianguloRetangulo.java
@@ -31,8 +31,66 @@ public class TrianguloRetangulo {
     public TrianguloRetangulo() {
     }
     
-    // TODO 2 crie aqui os métodos de configuração set e get de todos os atributos (dica: Alt+Insert)
-    // TODO 3 dentro de cada método set, implemente a seguinte regra de validação:
+    
+    public double getCateto1() {
+    	return cateto1;
+    }
+    
+    public void setCeteto1(double cateto1) {
+    	if( cateto1 > 0 ) {
+    		this.cateto1 = cateto1;
+    	} else {
+    		System.err.println("Valor Invalido");
+    		System.exit(0);
+    	}
+    	
+    }
+    
+    
+    public double getCateto2() {
+    	return cateto2;
+    }
+    
+    public void setCeteto2(double cateto2) {
+    	if( cateto2 > 0 ) {
+    		this.cateto2 = cateto2;
+    	} else {
+    		System.err.println("Valor Invalido");
+    		System.exit(0);
+    	}
+    }
+    
+    
+    public double getHipotenusa() {
+    	return hipotenusa;
+    }
+    
+    public void setHipotenusa(double hipotenusa) {
+    	if( hipotenusa > 0 ) {
+    		this.hipotenusa = hipotenusa;
+    	} else {
+    		System.err.println("Valor Invalido");
+    		System.exit(0);
+    	}
+    	
+    }
+    
+    
+    public double getAlturaHipotenusa() {
+    	return alturaHipotenusa;
+    }
+    
+    public void setAlturaHipotenusa(double alturaHipotenusa) {
+    	if( alturaHipotenusa > 0 ) {
+    		this.alturaHipotenusa = alturaHipotenusa;
+    	} else {
+    		System.err.println("Valor Invalido");
+    		System.exit(0);
+    	}
+    }
+    
+    // DONE 2 crie aqui os métodos de configuração set e get de todos os atributos (dica: Alt+Insert)
+    // DONE 3 dentro de cada método set, implemente a seguinte regra de validação:
     ///////// o atributo só será modificado se o valor passado para o set for maior que zero
     ///////// caso contrário, imprimir a mensagem "Valor inválido" e encerrar o programa
 
