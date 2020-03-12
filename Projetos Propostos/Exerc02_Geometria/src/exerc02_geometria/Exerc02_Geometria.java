dfec8d9c10a20c6be601a5677c26b9a992602e39
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-12 10:37:23 -0300

    TODO 4

--- Projetos Propostos/Exerc02_Geometria/src/exerc02_geometria/Exerc02_Geometria.java
+++ Projetos Propostos/Exerc02_Geometria/src/exerc02_geometria/Exerc02_Geometria.java
@@ -8,38 +8,34 @@ public class Exerc02_Geometria {
     public static void main(String[] args) {
         TrianguloRetangulo tr1 = new TrianguloRetangulo();
         
-        // TODO 4 apague as linhas de acesso direto aos atributos
-        ///////// sabemos que este tipo de acesso é prejudical para o projeto, porque ... (complete)
-        tr1.cateto1 = 6;
-        tr1.cateto2 = 5;
-        tr1.hipotenusa = 7;
-        tr1.alturaHipotenusa = 4.5;
+        // DONE 4 apague as linhas de acesso direto aos atributos
+        ///////// sabemos que este tipo de acesso Ã© prejudical para o projeto, porque diminui a segurança(complete)
         
-        // TODO 5 através do set, atribua valor 14.5 para o atributo cateto1
-        // TODO 6 através do set, atribua valor 48.1 para o atributo cateto2
-        // TODO 7 através do set, atribua valor 51.2 para o atributo hipotenusa
-        // TODO 8 através do set, atribua valor 12.3 para o atributo alturaHipotenusa
+        // TODO 5 atravÃ©s do set, atribua valor 14.5 para o atributo cateto1
+        // TODO 6 atravÃ©s do set, atribua valor 48.1 para o atributo cateto2
+        // TODO 7 atravÃ©s do set, atribua valor 51.2 para o atributo hipotenusa
+        // TODO 8 atravÃ©s do set, atribua valor 12.3 para o atributo alturaHipotenusa
         
-        // TODO 9 comente: as atribuições deram certo? Por quê?
+        // TODO 9 comente: as atribuiÃ§Ãµes deram certo? Por quÃª?
         
-        // TODO 10 crie aqui uma variável double de nome areaTr1
-        // TODO 11 através do objeto tr1, acesse o método calcArea() da classe TrianguloRetangulo 
-        ////////// e armazene o valor de retorno do método na variável areaTr1
-        // TODO 12 imprima o valor da área da seguinte forma: "Área do triângulo tr1: <VALOR>"
-        // TODO 13 comente: por que está dando erro?
+        // TODO 10 crie aqui uma variÃ¡vel double de nome areaTr1
+        // TODO 11 atravÃ©s do objeto tr1, acesse o mÃ©todo calcArea() da classe TrianguloRetangulo 
+        ////////// e armazene o valor de retorno do mÃ©todo na variÃ¡vel areaTr1
+        // TODO 12 imprima o valor da Ã¡rea da seguinte forma: "Ã�rea do triÃ¢ngulo tr1: <VALOR>"
+        // TODO 13 comente: por que estÃ¡ dando erro?
         // TODO 14 comente: o que pode ser feito para resolver este erro?
         
         
-        // TODO 16 crie aqui uma variável double de nome perimetroTr1
-        // TODO 17 através do objeto tr1, acesse o método calcPerimetro() da classe TrianguloRetangulo 
-        ////////// e armazene o valor de retorno do método na variável perimetroTr1
-        // TODO 18 imprima o valor do perímetro da seguinte forma: "Perímetro do triângulo tr1: <VALOR>"
-        // TODO 19 comente: por que está dando erro?
+        // TODO 16 crie aqui uma variÃ¡vel double de nome perimetroTr1
+        // TODO 17 atravÃ©s do objeto tr1, acesse o mÃ©todo calcPerimetro() da classe TrianguloRetangulo 
+        ////////// e armazene o valor de retorno do mÃ©todo na variÃ¡vel perimetroTr1
+        // TODO 18 imprima o valor do perÃ­metro da seguinte forma: "PerÃ­metro do triÃ¢ngulo tr1: <VALOR>"
+        // TODO 19 comente: por que estÃ¡ dando erro?
         // TODO 20 comente: o que pode ser feito para resolver este erro?
         
-        // TODO 21 crie um outro objeto através do construtor, passando um valor inválido
-        // TODO 22 através do objeto da tarefa anterior, chame os métodos calcArea() e calcPerimetro()
-        // TODO 23 imprima os valores dos cálculos
+        // TODO 21 crie um outro objeto atravÃ©s do construtor, passando um valor invÃ¡lido
+        // TODO 22 atravÃ©s do objeto da tarefa anterior, chame os mÃ©todos calcArea() e calcPerimetro()
+        // TODO 23 imprima os valores dos cÃ¡lculos
         
         // TODO 24 comente: o que acontece? o que fazer para consertar este problema?
         
