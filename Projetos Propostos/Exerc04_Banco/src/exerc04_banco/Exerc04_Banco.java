5f09f592426a84ec0011892561128f66e40ba1c0
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-26 09:46:25 -0300

    Exercício 04 com os TODO 1, 2 e 3

---
+++ Projetos Propostos/Exerc04_Banco/src/exerc04_banco/Exerc04_Banco.java
@@ -0,0 +1,22 @@
+
+package exerc04_banco;
+
+/*
+    DONE 01 - LEIA O OBJETIVO DO PROJETO:
+    O objetivo deste projeto é criar um programa para exemplificar os conceitos de herança em POO;
+    O cenário escolhido foi um banco;
+    A princípio, dois tipos de transações bancárias poderão ser realizadas: transferência e boleto bancário.
+*/
+
+public class Exerc04_Banco {
+
+    // DONE 02 - crie um pacote de nome br.banco.transacao
+    // DONE 03 - dentro de br.banco.transacao, crie uma classe chamada Transferencia 
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
