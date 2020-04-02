90eec214bb669621e5a996efe4be94de3f0b4f59
Diogo Felipe <dlcfelipp@gmail.com>
2020-04-02 14:28:50 -0300

    TODO 1

--- Projetos Propostos/Exerc08_Janelas/src/exerc08_janelas/Exerc08_Janelas.java
+++ Projetos Propostos/Exerc08_Janelas/src/exerc08_janelas/Exerc08_Janelas.java
@@ -1,10 +1,13 @@
 
 package exerc08_janelas;
 
+import janelas.JanelaLogin;
+
 public class Exerc08_Janelas {
 
     public static void main(String[] args) {
-        // TODO 01 - crie aqui um objeto da classe JanelaLogin de nome janelaLogin
+        // DONE 01 - crie aqui um objeto da classe JanelaLogin de nome janelaLogin
+    	JanelaLogin janelaLogin = new JanelaLogin();
         
         // TODO 02 - através do objeto janelaLogin, acesse o método setSize 
         ////////// e passe os seguintes valores: 300, 300
@@ -12,6 +15,8 @@ public class Exerc08_Janelas {
         ////////// e passe o seguinte valor: JFrame.EXIT_ON_CLOSE (realize importação)
         // TODO 04 - através do objeto janelaLogin, acesse o método setVisible 
         ////////// e passe o seguinte valor: true
+    	
+    	
         
         // TODO 05 - execute o projeto, digite valores nos campos e clique no botão!
         ////////// o que acontece?
