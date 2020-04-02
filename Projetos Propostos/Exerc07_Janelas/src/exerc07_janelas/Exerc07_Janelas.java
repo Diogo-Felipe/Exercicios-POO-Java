6bac8cd1a90c4e5b527f65efdef54bc538449dae
Diogo Felipe <dlcfelipp@gmail.com>
2020-04-02 14:12:58 -0300

    Adição dos projetos e realização dos TODO 1,2 e 3

---
+++ Projetos Propostos/Exerc07_Janelas/src/exerc07_janelas/Exerc07_Janelas.java
@@ -0,0 +1,20 @@
+
+package exerc07_janelas;
+
+/*
+    DONE 01 - O objetivo deste projeto é demonstrar a criação e o funcionamento 
+    de alguns componentes da Interface Gráfica do Usuário (GUI) com o pacote
+    Swing do Java.
+*/
+
+public class Exerc07_Janelas {
+
+    // DONE 02 - crie um pacote chamado janelas no projeto
+    // Done 03 - dentro do pacote janelas, crie uma classe de nome JanelaLogin
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Projetos Propostos/Exerc08_Janelas/src/exerc08_janelas/Exerc08_Janelas.java
@@ -0,0 +1,21 @@
+
+package exerc08_janelas;
+
+public class Exerc08_Janelas {
+
+    public static void main(String[] args) {
+        // TODO 01 - crie aqui um objeto da classe JanelaLogin de nome janelaLogin
+        
+        // TODO 02 - através do objeto janelaLogin, acesse o método setSize 
+        ////////// e passe os seguintes valores: 300, 300
+        // TODO 03 - através do objeto janelaLogin, acesse o método setDefaultCloseOperation 
+        ////////// e passe o seguinte valor: JFrame.EXIT_ON_CLOSE (realize importação)
+        // TODO 04 - através do objeto janelaLogin, acesse o método setVisible 
+        ////////// e passe o seguinte valor: true
+        
+        // TODO 05 - execute o projeto, digite valores nos campos e clique no botão!
+        ////////// o que acontece?
+        
+    }
+    
+}
